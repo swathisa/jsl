a88bca6f20acb7f3c2f28a4528f4802a3cf6ebc3
swathisa <58590949+swathisa@users.noreply.github.com>
2019-12-30 12:06:19 +0100

    check the git version

--- vars/pythonSetupPyPipeline.groovy
+++ vars/pythonSetupPyPipeline.groovy
@@ -238,6 +238,7 @@ def call(Map pipelineParams) {
                 script {
                   //check if "ghp-import" plugin is installed to deploy docs
                   //isDeployDocsPluginInstalled()
+                  sh "git --version"
                   sh "ghp-import -m \"Documentation update to $moduleVersion\" -p -b docs build/sphinx/html"
                   sh "git tag docs-$moduleVersion docs"
                   sh "git push origin docs --tags"
