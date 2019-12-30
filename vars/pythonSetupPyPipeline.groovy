e49b3f7a9c2d86711ef2f25ba20e92f5f09e0628
swathisa <58590949+swathisa@users.noreply.github.com>
2019-12-30 11:31:03 +0100

    remove the check for ghp-import

--- vars/pythonSetupPyPipeline.groovy
+++ vars/pythonSetupPyPipeline.groovy
@@ -237,7 +237,7 @@ def call(Map pipelineParams) {
               sshagent([pipelineParams.sshAgentUser]) {
                 script {
                   //check if "ghp-import" plugin is installed to deploy docs
-                  isDeployDocsPluginInstalled()
+                  //isDeployDocsPluginInstalled()
                   sh "ghp-import -m \"Documentation update to $moduleVersion\" -p -b docs build/sphinx/html"
                   sh "git tag docs-$moduleVersion docs"
                   sh "git push origin docs --tags"
