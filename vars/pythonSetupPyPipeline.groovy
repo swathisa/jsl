73e0ab22ab6b901a3850792eb55b7a91ddb4e6ea
swathisa <58590949+swathisa@users.noreply.github.com>
2020-01-03 16:10:58 +0100

    check if a new commit is added

--- vars/pythonSetupPyPipeline.groovy
+++ vars/pythonSetupPyPipeline.groovy
@@ -238,14 +238,18 @@ def call(Map pipelineParams) {
                   }
                 }
                 steps {
-                  sh "ghp-import -m \"Documentation update to $moduleVersion\" -b docs build/sphinx/html"
+                  withGitEnv([scmCredentialsId: pipelineParams.scmCredentialsId]) {
+                    sh "ghp-import -m \"Documentation update to $moduleVersion\" -b docs build/sphinx/html"
+                    sh "git checkout docs"
+                    sh "git log"
+                  }
                 }
               }
               stage("push docs") {
                 steps {
                   withGitEnv([scmCredentialsId: pipelineParams.scmCredentialsId]) {
                     sh "git tag docs-$moduleVersion docs"
-                    sh "git push -u origin docs --tags"
+                    sh "git push origin docs --tags"
                   }
                 }
               }
