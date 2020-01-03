feed5f4ba07050f560d3634fd95e5579a3df3223
swathisa <58590949+swathisa@users.noreply.github.com>
2020-01-03 14:13:22 +0100

    git checkout instead of fetch

--- vars/pythonSetupPyPipeline.groovy
+++ vars/pythonSetupPyPipeline.groovy
@@ -232,7 +232,7 @@ def call(Map pipelineParams) {
             steps {
               withGitEnv([scmCredentialsId: pipelineParams.scmCredentialsId]) {
                 script {
-                  sh "git fetch origin docs:docs"
+                  sh "git checkout docs"
                   sh "ghp-import -m \"Documentation update to $moduleVersion\" -p -b docs build/sphinx/html"
                   sh "git tag docs-$moduleVersion docs"
                   sh "git push origin docs --tags"
