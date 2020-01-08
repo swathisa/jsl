51cd76a9762557e15e664208a9258c4ca38d0348
swathisa <58590949+swathisa@users.noreply.github.com>
2020-01-08 18:24:44 +0100

    wrap the if within script

--- vars/pythonSetupPyPipeline.groovy
+++ vars/pythonSetupPyPipeline.groovy
@@ -237,10 +237,10 @@ def call(Map pipelineParams) {
             }
             steps {
               withGitEnv([scmCredentialsId: pipelineParams.scmCredentialsId]) {
-                if (sh(script: "git fetch origin docs:docs", returnStatus: true) != 0) {
-                  sh "git checkout --orphan docs"
-                }
                 script {
+                  if (sh(script: "git fetch origin docs:docs", returnStatus: true) != 0) {
+                    sh "git checkout --orphan docs"
+                  }
                   sh "ghp-import -m \"Documentation update to $moduleVersion\" -p -b docs build/sphinx/html"
                   sh "git tag docs-$moduleVersion docs"
                   sh "git push origin docs --tags"
